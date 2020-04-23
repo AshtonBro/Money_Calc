15d2653b9d1ab006390462b85fe0b0fd4d41b1e9
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-04-23 22:45:10 +0500

    renderOperation

--- js/script.js
+++ js/script.js
@@ -9,4 +9,47 @@ const totalBalance = document.querySelector('.total__balance'),
     operationAmount = document.querySelector('.operation__amount');
 
 
-let dbOperation = [];
+let dbOperation = [
+    {
+        id: '234fs2',
+        description: 'Получил зарплату',
+        amount: 30000,
+    },
+    {
+        id: 'fg24',
+        description: 'Купил куртку',
+        amount: -6412,
+    },
+    {
+        id: 'asd234asd',
+        description: 'Сходил за продуктами',
+        amount: -3500,
+    },
+    {
+        id: 'sdfsf23423',
+        description: 'Купил стройматериалы',
+        amount: -6000,
+    },
+    {
+        id: '2134as',
+        description: 'Заказ на фриланс',
+        amount: 10000,
+    },
+    {
+        id: '5sdf342',
+        description: 'Купил книги',
+        amount: -2890,
+    },
+];
+
+const renderOperation = () => {
+
+    const listItem = document.createElement('li');
+
+    listItem.classList.add('history__item');
+
+    listItem.innerHTML = ``;
+
+};
+
+renderOperation();
