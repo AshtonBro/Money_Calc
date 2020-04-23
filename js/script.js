4a3a9702cd1844d4f338b0c611c96aa23787edc1
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-04-23 22:32:03 +0500

    jshint

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-'use strict'
+'use strict';
+
+const totalBalance = document.querySelector('.total__balance'),
+    totalMoneyIncome = document.querySelector('.total__money-income'),
+    totalMoneyExpenses = document.querySelector('.total__money-expenses'),
+    historyList = document.querySelector('.history-list'),
+    form = document.querySelector('#form'),
+    operationName = document.querySelector('.operation__name'),
+    operationAmount = document.querySelector('.operation__amount');
+
+
+let dbOperation = [];
